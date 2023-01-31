880de4e843c6b78086e68a9d5b9164e913c1a264
Cristian Zavala <czavala.dev@gmail.com>
2023-01-31 16:25:09 -0300

    fix port

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 
+
 const { getApod } = require('./services');
 
 app.set('view engine', 'ejs'); // Setting 'EJS' as the 'view engine'
@@ -13,14 +14,15 @@ app.set('view engine', 'ejs'); // Setting 'EJS' as the 'view engine'
 app.get('/', async (req, res) => {
     const response = await getApod();
     const data = response.data;
-    if(!data.copyright) { // Agrega el valor unknown a copyright, api no siempre entrega ese valor
+    if(!data.copyright) { // Agrega el valor unknown a copyright en caso de no estar, api no siempre entrega ese valor
         data.copyright = 'Unknown';
     }
     console.log(data);
     res.render('index', data);
 })
 
-app.listen(3000, () => {
-    console.log('LISTENING ON PORT 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`LISTENING ON PORT ${port}`);
 })
 
